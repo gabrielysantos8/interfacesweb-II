6bfea4cc15a6049b6078da34cc891b4a5edd1f0d
Gabriely <159936039+gabrielysantos8@users.noreply.github.com>
2025-04-28 15:26:34 -0300

    Add files via upload

--- aula24_04/exercício2/script2.js
+++ aula24_04/exercício2/script2.js
@@ -1,6 +1,6 @@
 // Função chamada ao clicar no botão
 function mudarTexto() {
-    texto.textContent = "Texto atualizado com JavaScript!";;
+    texto.textContent = "Texto atualizado com JavaScript!";
 }
 
 // Atribui a função ao botão ao carregar a página
